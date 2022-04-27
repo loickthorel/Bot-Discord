73814163ff8ced82edfa2f250b40e405c6aaac9c
lthorel <loickthorel1999@gmail.com>
2022-04-27 20:56:38 +0200

    Console debug

--- Bot-Discord/AmazonSearch.cs
+++ Bot-Discord/AmazonSearch.cs
@@ -18,15 +18,20 @@ public class AmazonSearch
         var ret = new HttpRequestFluent(true)
             .FromUrl(url)
             .Load();
+        
+        Console.WriteLine("test-après ret");
 
         var byClassContain =
             ret.HtmlPage.GetByClassNameContains("sg-col-4-of-12 " +
                                                 "s-result-item s-asin sg-col-4-of-16 " +
                                                 "sg-col s-widget-spacing-small sg-col-4-of-20");
 
+        Console.WriteLine("test après class");
         var list = new List<AmazonObj>();
         foreach (var result in byClassContain)
         {
+            Console.WriteLine("foreach");
+            
             if (!result.InnerHtml.Contains(
                     "<i class=\"a-icon a-icon-prime a-icon-medium\" role=\"img\" aria-label=\"Amazon Prime\"></i>"))
                 continue;
